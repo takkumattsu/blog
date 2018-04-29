787b2de8d5a72bf7616c0bf927b11f3277b28cf2
TakkuMattsu <3356758+TakkuMattsu@users.noreply.github.com>
2018-04-30 00:35:43 +0900

    usaturn.css がインストールされないので手動でインストール

--- setup.sh
+++ setup.sh
@@ -4,3 +4,6 @@ python3 -m venv env
 . env/bin/activate
 pip install -r requirements.txt
 patch -p0 < usaturn.patch
+touch env/lib/python3.6/site-packages/sphinxjp/usaturn.css
+patch -p0 < usaturn.css.patch
+
